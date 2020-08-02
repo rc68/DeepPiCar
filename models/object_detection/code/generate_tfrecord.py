c6ee3114900a309bb3a708be207d4a7981afa738
Robert Chen <rc68@rice.edu>
2020-08-02 16:49:52 -0700

    tf2 compat

--- models/object_detection/code/generate_tfrecord.py
+++ models/object_detection/code/generate_tfrecord.py
@@ -24,7 +24,7 @@ from PIL import Image
 from object_detection.utils import dataset_util
 from collections import namedtuple, OrderedDict
 
-flags = tf.app.flags
+flags = tf.compat.v1.app.flags
 flags.DEFINE_string("csv_input", "", "Path to the CSV input")
 flags.DEFINE_string("output_path", "", "Path to output TFRecord")
 flags.DEFINE_string(
@@ -131,4 +131,4 @@ def main(_):
 
 
 if __name__ == "__main__":
-    tf.app.run()
+    tf.compat.v1.app.run()
