6a5ba65131ba291d65faa01e1f99eb0b9d5c8982
Robert Chen <rc68@rice.edu>
2020-08-02 18:29:41 -0700

    revert

--- models/object_detection/code/generate_tfrecord.py
+++ models/object_detection/code/generate_tfrecord.py
@@ -24,7 +24,7 @@ from PIL import Image
 from object_detection.utils import dataset_util
 from collections import namedtuple, OrderedDict
 
-flags = tf.compat.v1.app.flags
+flags = tf.app.flags
 flags.DEFINE_string("csv_input", "", "Path to the CSV input")
 flags.DEFINE_string("output_path", "", "Path to output TFRecord")
 flags.DEFINE_string(
@@ -50,7 +50,7 @@ def split(df, group):
 
 
 def create_tf_example(group, path, label_map):
-    with tf.compat.v1.gfile.GFile(os.path.join(path, "{}".format(group.filename)), "rb") as fid:
+    with tf.gfile.GFile(os.path.join(path, "{}".format(group.filename)), "rb") as fid:
         encoded_jpg = fid.read()
     encoded_jpg_io = io.BytesIO(encoded_jpg)
     image = Image.open(encoded_jpg_io)
@@ -80,8 +80,8 @@ def create_tf_example(group, path, label_map):
         )
         classes.append(class_index)
 
-    tf_example = tf.compat.v1.train.Example(
-        features=tf.compat.v1.train.Features(
+    tf_example = tf.train.Example(
+        features=tf.train.Features(
             feature={
                 "image/height": dataset_util.int64_feature(height),
                 "image/width": dataset_util.int64_feature(width),
@@ -104,7 +104,7 @@ def create_tf_example(group, path, label_map):
 
 
 def main(_):
-    writer = tf.compat.v1.python_io.TFRecordWriter(FLAGS.output_path)
+    writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
     path = os.path.join(os.getcwd(), FLAGS.img_path)
     examples = pd.read_csv(FLAGS.csv_input)
 
@@ -131,4 +131,4 @@ def main(_):
 
 
 if __name__ == "__main__":
-    tf.compat.v1.app.run()
+    tf.app.run()
